1a686aeed8e65e42aee8705d9028efe5adfd4971
efrainbss <efrainbss@gmail.com>
2024-03-25 13:11:21 -0700

    ultimos cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
-import { CounterModule } from './counter/counter.module';
-import { HeroeModule } from './heroes/components/heroe.module';
+// import { CounterModule } from './counter/counter.module';
+// import { HeroeModule } from './heroes/components/heroe.module';
+import { DbzModule } from './dbz/dbz.module';
+
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
@@ -15,8 +17,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CounterModule,
-    HeroeModule
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
